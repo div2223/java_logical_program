2a870d8c8a314c3d296c43ccd0cd24b8a6f5025f
div2223 <kaitepallinagadivya@gmail.com>
2024-11-07 22:11:47 +0530

    Add files via upload

---
+++ day 2/Big.java
@@ -0,0 +1,22 @@
+class Big
+{
+public static void main(String args[])
+{
+int a=20,b=40,c=60;
+if(a>b)
+{
+if(a>c)
+{
+System.out.println(" a is Big");
+}
+}
+else if(b>c)
+{
+System.out.println( "b is Big");
+}
+else
+{
+System.out.println("c is big");
+}
+}
+}
---
+++ day 2/EvenOdd.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class EvenOdd{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        
+        System.out.println("Enter a number: ");
+        n= sc.nextInt();
+        
+        if (n % 2 == 0) {
+            System.out.println(n + " is even.");
+        } else {
+            System.out.println(n + " is odd.");
+       
+        
+        sc.close();
+        }
+    }
+}
